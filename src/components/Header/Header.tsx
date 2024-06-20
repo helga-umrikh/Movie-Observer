15060586993b17c391f6f7ac1a970fab75ed1754
helga-umrikh <zetaumr@gmail.com>
2024-06-21 00:33:20 +0300

    fix: navigation in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import styles from './styles.module.scss'
 import { Header as HeaderComponent } from 'antd/es/layout/layout'
 import { Avatar, Typography } from 'antd'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Button } from '@mui/material'
 import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone'
+import { NavigationButton } from '../NavigationButton/NavigationButton'
 const { Title } = Typography
 
 const Header = () => {
+    const location = useLocation()
+    const currentPath = location.pathname
     return (
         <HeaderComponent className={styles.header}>
             <div>
@@ -24,17 +27,22 @@ const Header = () => {
                     </Title>
                 </Link>
             </div>
-            <Link to={'/favorites'}>
-                <Button
-                    variant="text"
-                    className={styles.header__button_favorite}
-                >
-                    Favorites
-                    <span className={styles.button__icon}>
-                        <FavoriteTwoToneIcon />
-                    </span>
-                </Button>
-            </Link>
+            <div>
+                {currentPath !== '/favorites' && (
+                    <Link to={'/favorites'}>
+                        <Button
+                            variant="text"
+                            className={styles.header__button_favorite}
+                        >
+                            Favorites
+                            <span className={styles.button__icon}>
+                                <FavoriteTwoToneIcon />
+                            </span>
+                        </Button>
+                    </Link>
+                )}
+                <NavigationButton route="/" />
+            </div>
         </HeaderComponent>
     )
 }
